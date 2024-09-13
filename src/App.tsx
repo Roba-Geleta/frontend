b6e25849504ae12dabf76aa4c93220f6520bdf49
Roba Geleta <geletaroba@yahoo.ca>
2024-09-13 14:31:24 -0500

    implement form, prop drilling, enter submit ep 12

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,17 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+  };
+
+  const onPortfolioCreate = (e: SyntheticEvent) => {
+    e.preventDefault();
     console.log(e);
   };
 
-  const onClick = async (e: SyntheticEvent) => {
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
     const result = await searchCompanies(search);
     if (typeof result === "string") {
       setServerError(result);
@@ -30,8 +35,15 @@ function App() {
   return (
     <>
       <div className="App">
-        <Search onClick={onClick} search={search} handleChange={handleChange} />
-        <CardList searchResults={searchResult} />
+        <Search
+          onSearchSubmit={onSearchSubmit}
+          search={search}
+          handleSearchChange={handleSearchChange}
+        />
+        <CardList
+          searchResults={searchResult}
+          onPortfolioCreate={onPortfolioCreate}
+        />
 
         {serverError && <h1>Unable to connect to API</h1>}
       </div>
