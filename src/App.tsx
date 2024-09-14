0469148c2d144e5565f02820c88c7cac10031915
Roba Geleta <geletaroba@yahoo.ca>
2024-09-14 16:15:29 -0500

    update arrays and build a list ep 13

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 
 function App() {
   // const [count, setCount] = useState(0);
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
@@ -15,9 +17,12 @@ function App() {
     setSearch(e.target.value);
   };
 
-  const onPortfolioCreate = (e: SyntheticEvent) => {
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    console.log(e);
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
   };
 
   const onSearchSubmit = async (e: SyntheticEvent) => {
@@ -40,6 +45,7 @@ function App() {
           search={search}
           handleSearchChange={handleSearchChange}
         />
+        <ListPortfolio portfolioValues={portfolioValues} />
         <CardList
           searchResults={searchResult}
           onPortfolioCreate={onPortfolioCreate}
