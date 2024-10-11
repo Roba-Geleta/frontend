3ebabfd903d4c52d375fe5d61ba805c7a3f4253d
Roba Geleta <geletaroba@yahoo.ca>
2024-10-11 15:53:38 -0500

    fix lint error for error handler

--- src/Helpers/ErrorHandler.tsx
+++ src/Helpers/ErrorHandler.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const handleError = (error: any) => {
+export const handleError = (error: unknown) => {
   if (axios.isAxiosError(error)) {
     const err = error.response;
     if (Array.isArray(err?.data.errors)) {
-      for (let val of err.data.errors) {
+      for (const val of err.data.errors) {
         toast.warning(val.description);
       }
     } else if (typeof err?.data.errors === "object") {
-      for (let e in err?.data.errors) {
+      for (const e in err?.data.errors) {
         toast.warning(err.data.errors[e][0]);
       }
     } else if (err?.data) {
