0961b625021b0f46e6f6b1ad2c3fff7723070974
Roba Geleta <geletaroba@yahoo.ca>
2024-10-15 21:08:08 -0500

    move fmp api access to gcp

--- src/api.tsx
+++ src/api.tsx
@@ -12,102 +12,110 @@ import {
 interface SearchResponse {
   data: CompanySearch[];
 }
+const FUNCTION_BASE_URL = import.meta.env.VITE_APP_FUNCTION_BASE_URL;
 
 export const searchCompanies = async (query: string) => {
-  const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
-  console.log("API_KEY: ", API_KEY);
-
   try {
-    const data = await axios.get<SearchResponse>(
-      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${API_KEY}`
+    const { data } = await axios.get<SearchResponse>(
+      `${FUNCTION_BASE_URL}/searchCompanies`,
+      {
+        params: { query },
+      }
     );
-
     return data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log("error message: ", error.message);
-      return error.message;
-    } else {
-      console.log("unexpected error: ", error);
-      return "An unexpected error has occured";
-    }
+  } catch (error: any) {
+    console.error("Error searching companies:", error.message);
+    throw error;
   }
 };
 
 export const getCompanyProfile = async (query: string) => {
+  console.log(FUNCTION_BASE_URL);
   try {
-    const data = await axios.get<CompanyProfile[]>(
-      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyProfile[]>(
+      `${FUNCTION_BASE_URL}/getCompanyProfile`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching company profile:", error.message);
+    throw error;
   }
 };
 
 export const getKeyMetrics = async (query: string) => {
   try {
-    const data = await axios.get<CompanyKeyMetrics[]>(
-      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyKeyMetrics[]>(
+      `${FUNCTION_BASE_URL}/getKeyMetrics`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching key metrics:", error.message);
+    throw error;
   }
 };
 
 export const getIncomeStatement = async (query: string) => {
   try {
-    const data = await axios.get<CompanyIncomeStatement[]>(
-      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyIncomeStatement[]>(
+      `${FUNCTION_BASE_URL}/getIncomeStatement`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching income statement:", error.message);
+    throw error;
   }
 };
 
 export const getBalanceSheet = async (query: string) => {
   try {
-    const data = await axios.get<CompanyBalanceSheet[]>(
-      `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?limit=40&apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyBalanceSheet[]>(
+      `${FUNCTION_BASE_URL}/getBalanceSheet`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching balance sheet:", error.message);
+    throw error;
   }
 };
 
 export const getCashflowStatement = async (query: string) => {
   try {
-    const data = await axios.get<CompanyCashFlow[]>(
-      `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=40&apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyCashFlow[]>(
+      `${FUNCTION_BASE_URL}/getCashflowStatement`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching cashflow statement:", error.message);
+    throw error;
   }
 };
 
 export const getTenK = async (query: string) => {
   try {
-    const data = await axios.get<CompanyTenK[]>(
-      `https://financialmodelingprep.com/api/v3/sec_filings/${query}?type=10-k&page=0&apikey=${
-        import.meta.env.VITE_REACT_APP_API_KEY
-      }`
+    const { data } = await axios.get<CompanyTenK[]>(
+      `${FUNCTION_BASE_URL}/getTenK`,
+      {
+        params: { query },
+      }
     );
     return data;
   } catch (error: any) {
-    console.log("error message: ", error.message);
+    console.error("Error fetching Ten-K filings:", error.message);
+    throw error;
   }
 };
