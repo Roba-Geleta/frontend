0b4b21ade4b16929780d5645f8b4634c5e9cb0bf
Roba Geleta <geletaroba@yahoo.ca>
2024-10-30 23:29:37 -0500

    remove .tsx reference in imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/Routes.tsx";
-import { ThemeProvider } from "./Context/ThemeContext.tsx";
-import { UserProvider } from "./Context/userAuth.tsx";
+import { router } from "./Routes/Routes";
+import { ThemeProvider } from "./Context/ThemeContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
