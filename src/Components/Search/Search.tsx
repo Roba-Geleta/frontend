6f9a8a66383785c44f651c00054642cd542e34f1
Roba Geleta <geletaroba@yahoo.ca>
2024-10-30 23:12:27 -0500

    stylize the search bar for stocks

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,33 +1,60 @@
 import React, { SyntheticEvent } from "react";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled } from "@mui/material/styles";
+import LinearProgress from "@mui/material/LinearProgress";
+import { Stack } from "@mui/material";
 
 interface Props {
   onSearchSubmit: (e: SyntheticEvent) => void;
   search: string;
   handleSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  loading: boolean;
 }
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: "2px 4px",
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+  maxWidth: 600,
+  margin: "16px 0",
+  backgroundColor:
+    theme.palette.mode === "dark" ? theme.palette.grey[800] : "#fff",
+}));
 
 const Search: React.FC<Props> = ({
   onSearchSubmit,
   search,
   handleSearchChange,
-}: Props): JSX.Element => {
+  loading,
+}): JSX.Element => {
   return (
-    <section className="relative bg-gray-100">
-      <div className="max-w-4xl mx-auto p-6 space-y-6">
-        <form
-          className="form relative flex flex-col w-full p-10 space-y-4 bg-darkBlue rounded-lg md:flex-row md:space-y-0 md:space-x-3"
-          onSubmit={onSearchSubmit}
-        >
-          <input
-            className="flex-1 p-3 border-2 rounded-lg placeholder-black focus:outline-none"
-            id="search-input"
-            placeholder="Search companies"
-            value={search}
-            onChange={handleSearchChange}
-          ></input>
-        </form>
-      </div>
-    </section>
+    <form
+      onSubmit={onSearchSubmit}
+      style={{ width: "100%" }}
+      className="flex flex-col items-center justify-center "
+    >
+      <StyledPaper elevation={3} className="relative">
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search companies"
+          inputProps={{ "aria-label": "search companies" }}
+          value={search}
+          onChange={handleSearchChange}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        {loading && (
+          <div className="w-full absolute bottom-0 left-0 rounded-b-md">
+            <LinearProgress />
+          </div>
+        )}
+      </StyledPaper>
+    </form>
   );
 };
 
