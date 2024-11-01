d43194c56344022b0eaf53ddf751637be998ca1a
Roba Geleta <geletaroba@yahoo.ca>
2024-10-31 19:26:22 -0500

    differ the hiding of logs from production to development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     target: "es2022",
@@ -9,6 +9,6 @@ export default defineConfig({
     sourcemap: false,
   },
   esbuild: {
-    drop: ["console", "debugger"],
+    drop: mode === "production" ? ["console", "debugger"] : [],
   },
-});
+}));
